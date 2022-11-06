24c6cc32445eb72b0e5e1ee98f2472364f7acb8d
SEBTO-D <112988593+SEBTO-D@users.noreply.github.com>
2022-11-06 15:31:19 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,4 +35,5 @@ def send_text(message):
         bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт", reply_markup=markup)
     elif message.text.lower()=='счётчики':
         bot.send_message(message.chat.id,'Наши счётчики считаю посителей и отправляют в удобном виде на сервер')
-        bot.polling()
+        
+    bot.infinity_polling()
