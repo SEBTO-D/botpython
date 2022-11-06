e7d7fd29fc039a74cabf5d26d3088698ec2a1c34
SEBTO-D <112988593+SEBTO-D@users.noreply.github.com>
2022-11-06 15:26:52 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,38 @@
-
 import telebot
 from telebot import types
 token='5760405967:AAHyGTM2JaA_CX1rikU4zzwKzpHOn0P3GNg'
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id,'Привет')
-@bot.message_handler(commands=['button'])
-def button_message(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Кнопка")
-    markup.add(item1)
-    bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
-@bot.message_handler(content_types='text')
-def message_reply(message):
-    if message.text=="Кнопка":
-        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1=types.KeyboardButton("Кнопка 2")
-        markup.add(item1)
-        bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
-    elif message.text=="Кнопка 2":
-        bot.send_message(message.chat.id,'Спасибо за прочтение статьи!')
-bot.infinity_polling()
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Сайт', callback_data=1))
+    markup.add(telebot.types.InlineKeyboardButton(text='Расскажи о счётчиках', callback_data=2))
+    markup.add(telebot.types.InlineKeyboardButton(text='Как могу записать на CustDev', callback_data=3))
+    bot.send_message(message.chat.id, text="Привет я бот Jetsell, чем я могу быть вам полезен?", reply_markup=markup)
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+
+    bot.answer_callback_query(callback_query_id=call.id )
+    answer = ''
+    if call.data == '1':
+        answer = 'https://jetsel.ru'
+    elif call.data == '2':
+        answer = 'Счётчики поситетелей отоброжат реальное кол-во людей '
+    elif call.data == '4':
+        answer = 'Записаться можно по ссылке\n https://forms.amocrm.ru/rlrlrcr'
+
+    bot.send_message(call.message.chat.id, answer)
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Ещё раз привет!')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Пока!')
+    elif message.text.lower ()=='сайт':
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://jetsel.ru')
+        markup.add(btn1)
+        bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт", reply_markup=markup)
+    elif message.text.lower()=='счётчики':
+        bot.send_message(message.chat.id,'Наши счётчики считаю посителей и отправляют в удобном виде на сервер')
+        bot.polling()
