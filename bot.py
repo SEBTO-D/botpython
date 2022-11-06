217a3ce40642c8f30b19fdcf62c25e3418f13344
SEBTO-D <112988593+SEBTO-D@users.noreply.github.com>
2022-11-06 14:49:08 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,24 @@
-import  telebot
-bot = telebot.TeleBot('5760405967:AAHyGTM2JaA_CX1rikU4zzwKzpHOn0P3GNg');
+
+import telebot
+from telebot import types
+token='5760405967:AAHyGTM2JaA_CX1rikU4zzwKzpHOn0P3GNg'
+bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,"Привет ✌️ ")
-bot.infinity_poling()
+    bot.send_message(message.chat.id,'Привет')
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=="Кнопка":
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Кнопка 2")
+        markup.add(item1)
+        bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+    elif message.text=="Кнопка 2":
+        bot.send_message(message.chat.id,'Спасибо за прочтение статьи!')
+bot.infinity_polling()
