a8dd0883fab32719d22304ebef78f0e382a7697a
SEBTO-D <112988593+SEBTO-D@users.noreply.github.com>
2022-11-06 13:12:42 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,6 @@
 import  telebot
 bot = telebot.TeleBot('5760405967:AAHyGTM2JaA_CX1rikU4zzwKzpHOn0P3GNg');
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет ✌️ ")
+bot.infinity_poling()
